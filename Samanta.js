239e34a3ed4deeef218f1d18913a724375477aa1
Michal Starski <michalstarski99@gmail.com>
2018-06-21 00:14:56 +0200

    a

--- Samanta.js
+++ Samanta.js
@@ -20,7 +20,7 @@ class Samanta {
     if (actions.HELLO.indexOf(text.toLowerCase()) >= 0) {
       const url = "https://graph.facebook.com/v2.6/me/messages?access_token=EAAGSikKKJqwBAFsNqmG0zWGSsP1ZBqfKHQ984pcZAR5nwv8AHCXNav5AFQSs4YOShZAEfkJR1vEZABTD6QuWN2wKwuQxnDU6V7TYBX9x9ApPoZBSpqbTZAAOabQg4HmsTIOrZBKjZBMZChj0RRJKZAH3ogDTZBZCXwYJjlIAFNEmTEYmlZAnZCwAKOtf1v";
       axios
-        .post(url, postData)
+        .post(url, this.postData)
         .then(response => {
           console.log(response.json());
         })
