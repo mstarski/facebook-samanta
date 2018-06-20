1e5bc1d95f176ca6e47ad587d6a9337cc7311fa1
michalStarski <michalstarski99@gmail.com>
2018-06-20 22:45:59 +0200

    a

--- Samanta.js
+++ Samanta.js
@@ -19,7 +19,7 @@ class Samanta {
 
   answer(text) {
     const postData = {
-      messaging_type: "text",
+      messaging_type: "RESPONSE",
       recipient: {
         id: "1687230281331060"
       },
@@ -30,7 +30,7 @@ class Samanta {
     const url =
       "https://graph.facebook.com/v2.6/me/messages?access_token=EAAGSikKKJqwBAFsNqmG0zWGSsP1ZBqfKHQ984pcZAR5nwv8AHCXNav5AFQSs4YOShZAEfkJR1vEZABTD6QuWN2wKwuQxnDU6V7TYBX9x9ApPoZBSpqbTZAAOabQg4HmsTIOrZBKjZBMZChj0RRJKZAH3ogDTZBZCXwYJjlIAFNEmTEYmlZAnZCwAKOtf1v";
     axios
-      .post('http://192.168.0.12:8080/', postData)
+      .post(url, postData)
       .then(response => {
         console.log(response.json());
 
