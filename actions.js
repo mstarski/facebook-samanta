7575a1dec0a2cb63c7f252e5ffac658584326812
Michal Starski <michalstarski99@gmail.com>
2018-06-21 00:14:02 +0200

    a

---
+++ actions.js
@@ -0,0 +1,15 @@
+const actions = {
+    HELLO: [
+        "czesc",
+        "cześć",
+        "cześc",
+        "czesć",
+        "witaj",
+        "hi",
+        "hello",
+        "hello there",
+        "hey"
+      ]
+}
+
+module.exports = actions;
