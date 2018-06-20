9c0f3e5e667d6a7196c952822e4ea59ae11fab50
Michal Starski <michalstarski99@gmail.com>
2018-06-21 00:25:08 +0200

    a

--- Samanta.js
+++ Samanta.js
@@ -17,6 +17,8 @@ class Samanta {
   }
 
   answer(text) {
+    if(text === undefined)
+      return;
     if (actions.HELLO.indexOf(text.toLowerCase().trim().replace(/\s\s+/g, ' ')) >= 0) {
       const url = "https://graph.facebook.com/v2.6/me/messages?access_token=EAAGSikKKJqwBAFsNqmG0zWGSsP1ZBqfKHQ984pcZAR5nwv8AHCXNav5AFQSs4YOShZAEfkJR1vEZABTD6QuWN2wKwuQxnDU6V7TYBX9x9ApPoZBSpqbTZAAOabQg4HmsTIOrZBKjZBMZChj0RRJKZAH3ogDTZBZCXwYJjlIAFNEmTEYmlZAnZCwAKOtf1v";
       axios
