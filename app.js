89468c5310f36900477efe0f941d9a5f5daea1ce
Michal Starski <michalstarski99@gmail.com>
2018-06-21 00:26:50 +0200

    a

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.post('/webhook', (req, res) => {
         // Gets the message. entry.messaging is an array, but 
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
+        if(webhook_event.message.text === undefined)
+          return;
         let text = webhook_event.message.text;
         console.log(text);
         Sam.answer(text);
