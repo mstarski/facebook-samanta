4c2495da57693058f7f90159a935f9015a75277b
michalStarski <michalstarski99@gmail.com>
2018-06-20 22:47:51 +0200

    a

--- Samanta.js
+++ Samanta.js
@@ -18,24 +18,26 @@ class Samanta {
   }
 
   answer(text) {
-    const postData = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: "1687230281331060"
-      },
-      message: {
-        text: "hello, world!"
-      }
-    };
-    const url =
-      "https://graph.facebook.com/v2.6/me/messages?access_token=EAAGSikKKJqwBAFsNqmG0zWGSsP1ZBqfKHQ984pcZAR5nwv8AHCXNav5AFQSs4YOShZAEfkJR1vEZABTD6QuWN2wKwuQxnDU6V7TYBX9x9ApPoZBSpqbTZAAOabQg4HmsTIOrZBKjZBMZChj0RRJKZAH3ogDTZBZCXwYJjlIAFNEmTEYmlZAnZCwAKOtf1v";
-    axios
-      .post(url, postData)
-      .then(response => {
-        console.log(response.json());
-
-      })
-      .catch(error => console.log(error.message));
+    switch (text) {
+      case this.HELLO.indexOf(text) !== -1:
+        const postData = {
+          messaging_type: "RESPONSE",
+          recipient: {
+            id: "1687230281331060"
+          },
+          message: {
+            text: "hello, world!"
+          }
+        };
+        const url =
+          "https://graph.facebook.com/v2.6/me/messages?access_token=EAAGSikKKJqwBAFsNqmG0zWGSsP1ZBqfKHQ984pcZAR5nwv8AHCXNav5AFQSs4YOShZAEfkJR1vEZABTD6QuWN2wKwuQxnDU6V7TYBX9x9ApPoZBSpqbTZAAOabQg4HmsTIOrZBKjZBMZChj0RRJKZAH3ogDTZBZCXwYJjlIAFNEmTEYmlZAnZCwAKOtf1v";
+        axios
+          .post(url, postData)
+          .then(response => {
+            console.log(response.json());
+          })
+          .catch(error => console.log(error.message));
+    }
   }
 }
 
