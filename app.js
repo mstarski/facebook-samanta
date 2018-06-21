006b5d98ee1df63a729b4cc94a8b85e4a02458ae
michalStarski <michalstarski99@gmail.com>
2018-06-21 22:38:21 +0200

    a

--- app.js
+++ app.js
@@ -36,6 +36,15 @@ app.post('/webhook', (req, res) => {
         console.log(webhook_event);
         console.log(text);
         const url = "https://graph.facebook.com/v2.6/me/messages?access_token=EAAGSikKKJqwBAFsNqmG0zWGSsP1ZBqfKHQ984pcZAR5nwv8AHCXNav5AFQSs4YOShZAEfkJR1vEZABTD6QuWN2wKwuQxnDU6V7TYBX9x9ApPoZBSpqbTZAAOabQg4HmsTIOrZBKjZBMZChj0RRJKZAH3ogDTZBZCXwYJjlIAFNEmTEYmlZAnZCwAKOtf1v";
+        const data =  {
+          messaging_type: "RESPONSE",
+          recipient: {
+            id: senderId
+          },
+          message: {
+            text: "hi"
+          }
+        };
         axios
           .post(url, data)
           .then(response => console.log('Message sent back! ' + data.message.text))
