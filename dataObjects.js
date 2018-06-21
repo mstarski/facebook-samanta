970091874696ea9a51c2f1c853a1888a387ffbe3
michalStarski <michalstarski99@gmail.com>
2018-06-21 13:10:43 +0200

    a

--- dataObjects.js
+++ dataObjects.js
@@ -31,12 +31,6 @@ module.exports = {
       text: "Here is a quick reply!",
       quick_replies: [
         {
-          content_type: "text",
-          title: "Search",
-          payload: "<POSTBACK_PAYLOAD>",
-          image_url: "http://example.com/img/red.png"
-        },
-        {
           content_type: "location"
         }
       ]
