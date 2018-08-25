8f987e3ccc0f392d6dc0541d2fee5384d1e07f48
michalStarski <michalstarski99@gmail.com>
2018-08-25 19:33:39 +0200

    checkpoint

--- scripts/zditm_scrapper.js
+++ scripts/zditm_scrapper.js
@@ -16,7 +16,7 @@ module.exports = props => {
 	let request = new Object();
 	console.log(params.substring(3, 6));
 
-	console.log(typeof parseInt(params.substring(3, 6)));
+	console.log(isNan(parseInt(params.substring(3, 6))));
 
 	if (isNan(parseInt(params.substring(3, 6)))) {
 		return "Podany numer linii jest błędny!";
