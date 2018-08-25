bc0a48f538b8ff363279842da9744dc9b2337f2a
michalStarski <michalstarski99@gmail.com>
2018-08-25 19:06:05 +0200

    checkpoint

--- scripts/zditm_scrapper.js
+++ scripts/zditm_scrapper.js
@@ -1,12 +1,14 @@
 const axios = require("axios");
+const cheerio = require("cheerio");
 
 module.exports = props => {
-    const params = props.replace(/\s/g, "");
-    console.log(params);
+	const params = props.replace(/\s/g, "");
+	console.log(params);
 	axios
 		.get("https://www.zditm.szczecin.pl/pasazer/rozklady-jazdy,linia,9")
 		.then(response => {
-			console.log(response.data);
+			const $ = cheerio.load(response.data);
+			console.log($("tbody > tr").text());
 		})
 		.catch(error => {
 			console.log(error);
