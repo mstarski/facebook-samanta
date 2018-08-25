9684232097a3c9011c17d1bf9382ceb034979440
michalStarski <michalstarski99@gmail.com>
2018-08-25 20:07:46 +0200

    checkpoint

---
+++ definitions/stickerUrls.js
@@ -0,0 +1,32 @@
+module.exports = {
+    stickers: [
+        {
+            name: 'cat_popcorn',
+            url: 'https://scontent.xx.fbcdn.net/v/t39.1997-6/p100x100/851536_272701746237424_61026026_n.png?_nc_cat=0&_nc_ad=z-m&_nc_cid=0&oh=ba7d7872500c114ea6bdc087460b86d5&oe=5BA8ADB1'
+        },
+        {
+            name: 'cpt_america',
+            url: 'https://scontent.xx.fbcdn.net/v/t39.1997-6/p100x100/29918859_198228964120121_6369323714126807040_n.png?_nc_cat=0&_nc_ad=z-m&_nc_cid=0&oh=e9625d4322620519cc4ca2631e5a2546&oe=5BE6E1C1'
+        },
+        {
+            name: 'rabbit_boring',
+            url: 'https://scontent.xx.fbcdn.net/v/t39.1997-6/p100x100/28418728_184002755550713_6055188006506070016_n.png?_nc_cat=0&_nc_ad=z-m&_nc_cid=0&oh=15793d5dd8f4260439e52bbf83062287&oe=5BA8FF09'
+        },
+        {
+            name: 'cat_computer',
+            url: 'https://scontent.xx.fbcdn.net/v/t39.1997-6/p100x100/10173508_272699406237658_713133850_n.png?_nc_cat=0&_nc_ad=z-m&_nc_cid=0&oh=7079b17c0e5769f11a58bbe8cd873c16&oe=5BA8C393'
+        },
+        {
+            name: 'toaster',
+            url: 'https://scontent.xx.fbcdn.net/v/t39.1997-6/p100x100/26655658_167803600648258_6548664256869433344_n.png?_nc_cat=0&_nc_ad=z-m&_nc_cid=0&oh=1be488f2c160024986249d1d78afdeb2&oe=5BEB6B1F'
+        },
+        {
+            name: 'cat_sushi',
+            url: 'https://scontent.xx.fbcdn.net/v/t39.1997-6/p100x100/851540_272702829570649_1475292467_n.png?_nc_cat=0&_nc_ad=z-m&_nc_cid=0&oh=d8c90fe4b292584e42f6c9bd28730101&oe=5BA78084'
+        }
+    ],
+    messageUnknown: {
+        name: 'cat_bewildered',
+        url: 'https://scontent.xx.fbcdn.net/v/t39.1997-6/p100x100/851567_272702672903998_299287436_n.png?_nc_cat=0&_nc_ad=z-m&_nc_cid=0&oh=0505da14768f127bd9baa88ec7b2770a&oe=5BB35029'
+    },
+}
