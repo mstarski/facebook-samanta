a70e31c17bc609ba3d0488964408bbd703424d6b
michalStarski <michalstarski99@gmail.com>
2018-08-27 16:36:46 +0200

    checkpoint

--- scripts/zditm_scrapper.js
+++ scripts/zditm_scrapper.js
@@ -21,7 +21,7 @@ function zditm_scrap(stop_name, line_number, self, senderId) {
 					.eq(6)
 					.text();
 				const departure = $("#najkursxhr").text();
-				return `*${direction}*\n ------------------- \n*${departure}*`;
+				return `*${direction}*\n*${departure}*\n-------------------`;
 			});
 	}
 
