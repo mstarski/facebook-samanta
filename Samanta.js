db98b602feefe4f6fed90c121becec66293c12bb
michalStarski <michalstarski99@gmail.com>
2018-08-27 15:54:13 +0200

    checkpoint

--- Samanta.js
+++ Samanta.js
@@ -81,13 +81,13 @@ class Samanta {
 
         else if(actions.ZDITM.indexOf(formattedText.substring(0,5)) >= 0) {
             function zditm() {
-                return new Promise((resolve, reject) => {
+                return new Promise((resolve, reject, samanta) => {
                     this.postTextMessage.message.text = zditm_scrapper(formattedText.substring(5));
                     resolve();
                 }) 
             };
             
-            zditm().then(() => {
+            zditm(this).then(() => {
                 this.postTextMessage.recipient.id = senderId;
                 this.submit(this.postTextMessage);
             })
