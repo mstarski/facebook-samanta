9e149e852813f0694951d3e4d472c1af5567625a
michalStarski <michalstarski99@gmail.com>
2018-08-27 15:29:17 +0200

    checkpoint

--- scripts/zditm_scrapper.js
+++ scripts/zditm_scrapper.js
@@ -2,11 +2,11 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const zditm_ids = require("../definitions/zditm_ids");
 
-async function zditm_scrap(stop_name, line_number) {
+function zditm_scrap(stop_name, line_number) {
 	const stopId = zditm_ids.stops_ids[stop_name][0];
 	const lineId = zditm_ids.line_ids[line_number];
 
-	await axios
+	axios
 		.get(
 			`https://www.zditm.szczecin.pl/pasazer/rozklady-jazdy,tabliczka,${lineId},${stopId}`
 		)
@@ -37,7 +37,7 @@ module.exports = props => {
 	}
 
 	if (params.indexOf("sow") !== -1) {
-		return zditm_scrap("Sowinskiego", line);
+		zditm_scrap("Sowinskiego", line);
 	}
 	if (params.indexOf("swo") !== -1) {
 		return "Swolezerow";
