2abd644a0f51a09169826b7f4f2562cd3660f3f9
Michal Starski <michalstarski99@gmail.com>
2018-12-13 15:05:41 +0100

    changed file names to match new host place

--- facebook-samanta.js
+++ facebook-samanta.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 const app = require("express")();
 const http = require("http").Server(app);
 const bodyParser = require("body-parser");
@@ -29,6 +30,37 @@ const Sam = new Samanta(credentials["page-access-token"]);
 app.get("/", (req, res) => {
 	res.send("Samanta - a facebook messenger bot");
 });
+
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "reggaeshark"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
+
+
 
 // Creates the endpoint for our webhook
 app.post("/webhook", (req, res) => {
---
+++ scripts/sam-mreducer.js
@@ -0,0 +1,93 @@
+let matrix = [];
+
+//Sorting function
+function matrixSort(a, b) {
+	let aZeros = 0;
+	let bZeros = 0;
+	for (let element of a) if (element === 0) aZeros++;
+	for (let element of b) if (element === 0) bZeros++;
+	if (aZeros < bZeros) return -1;
+	if (aZeros > bZeros) return 1;
+	return 0;
+}
+
+function reduce(n) {
+	//Find first non-zero element in the first row
+	let anchor = {};
+	for (let i = 0; i < matrix[n].length; i++)
+		if (matrix[n][i] !== 0) {
+			anchor.val = matrix[n][i];
+			anchor.i = n;
+			anchor.j = i;
+			break;
+		}
+	//Reduce it to 1 by dividing the whole row by that anchor
+	if (anchor.val !== undefined)
+		matrix[n] = matrix[n].map(e => e / anchor.val);
+
+	//Reduce all values below anchor to 0
+	for (let i = anchor.i + 1; i < matrix.length; i++) {
+		element = matrix[i][anchor.j];
+		for (let x = 0; x < matrix[i].length; x++) {
+			matrix[i][x] = matrix[i][x] - element * matrix[anchor.i][x];
+		}
+	}
+}
+
+function completeReduce() {
+	//Reduce all element above the leading 1 to 0
+	let reducer;
+	for (let i = 1; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			let level = 0;
+			//Get the leading 1
+			if (matrix[i][j] === 1) {
+				level = i - 1;
+				//climb up until the first row
+				do {
+					//Reducer is a element above 1 that will be used for row reduction and later reduced to 0
+					reducer = matrix[level][j];
+					//Row reduction
+					for (let x = 0; x < matrix[level].length; x++) {
+						matrix[level][x] -= reducer * matrix[i][x];
+					}
+					level = level - 1;
+				} while (level >= 0);
+				break;
+			}
+		}
+	}
+}
+
+function MatrixReduce(input) {
+	let args = input.split(".");
+	const rows = args[0];
+	const cols = args[1];
+	args = args.slice(2);
+
+	console.log(rows, cols, args);
+
+	for (let i = 0; i < rows; i++) {
+		const row = args.slice(0, cols);
+		if (row.length === 0) {
+			return false;
+		}
+		args = args.slice(cols);
+		matrix.push(row);
+	}
+	//Sort first
+	matrix.sort(matrixSort);
+
+	//Reduce
+	for (let n = 0; n < matrix.length; n++) reduce(n);
+	completeReduce();
+	//Sort once again if all 0 rows are not at the bottom of the matrix
+	matrix.sort();
+	for (let row of matrix) {
+		row = row.map(e => e.toPrecision(2));
+		console.log(row);
+	}
+	return matrix;
+}
+
+MatrixReduce("3.3.1.2.2.3.4.5.1.3.4");
