6dbc43127a7e73064b45643b2d30e64bce20a4c3
Michał Starski <michalstarski99@gmail.com>
2019-06-07 14:42:58 +0200

    test

--- facebook-samanta.js
+++ facebook-samanta.js
@@ -7,6 +7,12 @@ const router = express.Router();
 
 const facebook = require("./webhooks/facebook")(router);
 const github = require("./webhooks/github")(router);
+const sendWeather = require("../scripts/sendWeather");
+const Samanta = require("../Samanta/Samanta");
+const credentials = require("../utils/get-credentials");
+
+//Create Samanta Object that will response to user requests
+const Sam = new Samanta(credentials["page-access-token"]);
 
 //Middleware
 app.use(bodyParser.json());
@@ -14,6 +20,48 @@ app.use(bodyParser.json());
 //Webhooks
 app.use("/facebook", facebook);
 app.use("/github", github);
+
+// Creates the endpoint for our webhook
+app.post("/facebook/webhook", (req, res) => {
+	console.log("Im here at the post");
+	let body = req.body;
+	console.log(body);
+
+	// Checks this is an event from a page subscription
+	if (body.object === "page") {
+		// Iterates over each entry - there may be multiple if batched
+		body.entry.forEach(function(entry) {
+			// Gets the message. entry.messaging is an array, but
+			// will only ever contain one message, so we get index 0
+			let webhook_event = entry.messaging[0];
+			let senderId = webhook_event.sender.id;
+			let text = webhook_event.message.text;
+			let attachments = webhook_event.message.attachments;
+			console.log(webhook_event);
+
+			if (text) {
+				console.log(webhook_event);
+				console.log(text);
+
+				Sam.sendFacebookMessage(text, senderId);
+			} else if (attachments[0].payload.url) {
+				Sam.sendSticker(senderId);
+			} else if (attachments[0].type === "location") {
+				console.log(attachments[0].payload.coordinates);
+				const lat = attachments[0].payload.coordinates.lat;
+				const long = attachments[0].payload.coordinates.long;
+				sendWeather(senderId, lat, long, Sam);
+			} else {
+				Sam.messageUnknown(senderId);
+			}
+		});
+		// Returns a '200 OK' response to all requests
+		res.status(200).send("EVENT_RECEIVED");
+	} else {
+		// Returns a '404 Not Found' if event is not from a page subscription
+		res.sendStatus(404);
+	}
+});
 
 // Get will trigger Samanta's docs
 app.get("/", (req, res) => {
--- webhooks/facebook.js
+++ webhooks/facebook.js
@@ -1,10 +1,6 @@
 const credentials = require("../utils/get-credentials");
-const sendWeather = require("../scripts/sendWeather");
-const Samanta = require("../Samanta/Samanta");
 
 module.exports = function(router) {
-	//Create Samanta Object that will response to user requests
-	const Sam = new Samanta(credentials["page-access-token"]);
 
 	// Adds support for the webhook's GET method [Verification Purposes]
 	router.get("/webhook", (req, res) => {
@@ -32,57 +28,5 @@ module.exports = function(router) {
 		}
 	});
 
-	// Creates the endpoint for our webhook
-	router.post("/webhook", (req, res) => {
-		console.log("Im here!");
-		// Your verify token. Should be a random string.
-		let VERIFY_TOKEN = credentials["verify-token"];
-
-		// Parse the query params
-		let mode = req.query["hub.mode"];
-		let token = req.query["hub.verify_token"];
-		let challenge = req.query["hub.challenge"];
-
-		let body = req.body;
-
-		console.log(body);
-
-		// Checks this is an event from a page subscription
-		if (body.object === "page") {
-			// Iterates over each entry - there may be multiple if batched
-			body.entry.forEach(function(entry) {
-				// Gets the message. entry.messaging is an array, but
-				// will only ever contain one message, so we get index 0
-
-				console.log(entry);
-
-				let webhook_event = entry.messaging[0];
-				let senderId = webhook_event.sender.id;
-				let text = webhook_event.message.text;
-				let attachments = webhook_event.message.attachments;
-
-				if (text) {
-					console.log(webhook_event);
-					console.log(text);
-
-					Sam.sendFacebookMessage(text, senderId);
-				} else if (attachments[0].payload.url) {
-					Sam.sendSticker(senderId);
-				} else if (attachments[0].type === "location") {
-					console.log(attachments[0].payload.coordinates);
-					const lat = attachments[0].payload.coordinates.lat;
-					const long = attachments[0].payload.coordinates.long;
-					sendWeather(senderId, lat, long, Sam);
-				} else {
-					Sam.messageUnknown(senderId);
-				}
-			});
-			// Returns a '200 OK' response to all requests
-			res.status(200).send("EVENT_RECEIVED");
-		} else {
-			// Returns a '404 Not Found' if event is not from a page subscription
-			res.sendStatus(404);
-		}
-	});
 	return router;
 };
