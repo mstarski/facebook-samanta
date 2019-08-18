fb8cead26641336474bb7f9ead79589ef4626432
Michał Starski <michalstarski99@gmail.com>
2019-08-18 18:08:43 +0200

    test-deploy

--- scripts/poznan-mpk-api.js
+++ scripts/poznan-mpk-api.js
@@ -1,21 +1,26 @@
 const axios = require("axios");
+const qs = require("qs");
 
 async function ztm_quick_look(stop, line) {
 	const data = await axios
-		.get("127.0.0.1:4567/api/quick_look", {
-			stop,
-			line,
-		})
+		.get(
+			`127.0.0.1:4567/api/quick_look?${qs.stringify({
+				stop,
+				line,
+			})}`
+		)
 		.catch(e => console.error(e));
 	console.log(data);
 }
 
 async function ztm_get_routes(from, to) {
 	const data = await axios
-		.get("127.0.0.1:4567/api/get_routes", {
-			from,
-			to,
-		})
+		.get(
+			`127.0.0.1:4567/api/get_routes?${qs.stringify({
+				from,
+				to,
+			})}`
+		)
 		.catch(e => console.error(e));
 	console.log(data);
 }
