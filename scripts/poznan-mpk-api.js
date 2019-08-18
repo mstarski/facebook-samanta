605954d1d56ed52854ecf9b2fdae66f4ad466979
Michał Starski <michalstarski99@gmail.com>
2019-08-18 21:35:01 +0200

    test-deploy

--- scripts/poznan-mpk-api.js
+++ scripts/poznan-mpk-api.js
@@ -4,10 +4,12 @@ const qs = require("qs");
 async function ztm_quick_look(stop, line) {
 	const data = await axios
 		.get(
-			`hanabi.sealcode.org/poznan-mpk-api/api/quick_look?${qs.stringify({
-				stop,
-				line,
-			})}`
+			`https://hanabi.sealcode.org/poznan-mpk-api/api/quick_look?${qs.stringify(
+				{
+					stop,
+					line,
+				}
+			)}`
 		)
 		.catch(e => console.error(e));
 	console.log(data);
@@ -16,10 +18,12 @@ async function ztm_quick_look(stop, line) {
 async function ztm_get_routes(from, to) {
 	const data = await axios
 		.get(
-			`hanabi.sealcode.org/poznan-mpk-api/api/get_routes?${qs.stringify({
-				from,
-				to,
-			})}`
+			`https://hanabi.sealcode.org/poznan-mpk-api/api/get_routes?${qs.stringify(
+				{
+					from,
+					to,
+				}
+			)}`
 		)
 		.catch(e => console.error(e));
 	console.log(data);
